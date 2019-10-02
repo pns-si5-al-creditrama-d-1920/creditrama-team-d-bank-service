a43c6f96f2e76eb02ee1e82c2a7a0adf52c329c9
Alexis Gardin <alexis.gardin@univ-cotedazur.fr>
2019-10-02 20:51:12 +0200

    init project'

---
+++ src/test/java/com/unice/polytech/creditrama/teamd/bankservice/BankserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unice.polytech.creditrama.teamd.bankservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
